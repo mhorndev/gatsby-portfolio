32a752a549a3eb3a4e7ab12ffb08bc1af63b0b29
Mark Horn <mhorn.dev@gmail.com>
2020-12-27 03:57:46 -0500

    about

--- src/pages/about.js
+++ src/pages/about.js
@@ -2,11 +2,142 @@ import React, {useContext } from "react"
 import styled from "styled-components"
 import { Context } from "../components/context"
 
+const Page = styled.div`
+  top: 0; bottom: 0;
+  left: 0; right: 0;
+  overflow-x: none;
+`
+
+const Container = styled.div`
+  margin: 0 auto;
+  padding: 20px; 
+  max-width: 1000px;
+`
+
+const Section = styled.div`
+  width: 100%;
+  border-bottom: 1px solid #000;
+  @media (min-width: 768px) { 
+  display: ${
+    props => props.flex 
+      ? "flex" 
+      : "block"
+    }; 
+  }
+`
+
+const Header = styled.h1`
+  width: 100%;
+`
+
+const Accent = styled.span`
+  color: ${props => props.theme.accent};
+`
+
+const Description = styled.div`
+  flex: ${props => props.flex};
+`
+
+const Content = styled.div`
+  flex: ${props => props.flex};
+`
+
+const Label = styled.h3`
+  color: #999999;
+`
+
+const Company = styled.h3`
+  margin-bottom: 0;
+`
+
+const Position = styled.div`
+  margin-top: 0;
+  margin-bottom: 0;
+`
+
+const Timeframe = styled.div`
+  margin-top: 0;
+`
+
+const School = styled.h3`
+  margin-bottom: 0;
+`
+
+const Degree = styled.div`
+  margin-top: 0;
+  margin-bottom: 0;
+`
+
+const List = styled.ul`
+  margin-left: 0; 
+  padding-left: 0;
+  list-style-type: none;
+`
+
+const Item = styled.li`
+  &:before {
+    content: "• ";
+    font-weight: bold;
+    color: ${props => props.theme.accent};
+  }
+`
+
 const AboutPage = ({}) => {
   const {globalContext,setGlobalContext} = useContext(Context)
 
   return (
-    <div id="page">
+    <Page>
+      <Container>
+        <Header>
+          About{" "}<Accent>Me</Accent>.
+        </Header>
+        <Section flex>
+          <Description flex={2}>
+            <Label>Experience<Accent>.</Accent></Label>
+          </Description>
+          <Content flex={4}>
+            <Company>Streamline</Company>
+            <Position>Full Stack Developer</Position>
+            <Timeframe>2016 - Present</Timeframe>
+            <List>
+              <Item>abc</Item>
+            </List>
+          </Content>
+        </Section>
+        <Section flex>
+          <Description flex={2}>
+            <Label>Education<Accent>.</Accent></Label>
+          </Description>
+          <Content flex={4}>
+            <School>Western Governor's University</School>
+            <Degree>BSCS - Bachelor of Science, Computer Science</Degree>
+            <Timeframe>2018 - 2020</Timeframe>
+            <List>
+              <Item>abc</Item>
+            </List>
+          </Content>
+        </Section>
+        <Section flex>
+          <Description flex={2}>
+            <Label>Certifications<Accent>.</Accent></Label>
+          </Description>
+          <Content flex={4}>
+          <Company>Project+</Company>
+            <Position>Full Stack Developer</Position>
+            <Timeframe>2018</Timeframe>
+            <List>
+              <Item>abc</Item>
+            </List>
+          </Content>
+        </Section>
+      </Container>
+    </Page>
+  )
+}
+
+export default AboutPage
+
+{/* 
       <section className={globalContext.darkMode && "dark"}>
         <div id="container">
           <div className="section-title">
@@ -15,6 +146,7 @@ const AboutPage = ({}) => {
           <div className="section-content">Content</div>
         </div>
       </section>
+
       <section className={globalContext.darkMode ? "dark alt" : "alt"}>
         <div id="container">
           <div className="section-title"><h1>Experience.</h1></div>
@@ -86,8 +218,4 @@ const AboutPage = ({}) => {
           </div>
         </div>
       </section>
-    </div>
-  )
-}
-
-export default AboutPage
+      */}
